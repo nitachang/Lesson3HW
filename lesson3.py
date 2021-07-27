6671013d102d5c020547c3f83f1117d6bd078169
nitachang <80136827+nitachang@users.noreply.github.com>
2021-07-27 11:38:24 +0800

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 21 11:22:37 2021
+
+@author: nitac
+"""
+
+import random
+x=random.randint(1,5)
+print(x)
+
+
+import random
+x=random.randint(1,10)
+y=input('1到10猜1個數字')
+y=int(y)
+if x == y:
+    print('猜對了!')
+else:
+    print('猜錯了!')
+    print(x)
+ 
+    
+    
+x=1
+while x<20:
+    print('x',x)
+    x=x+2
+    
+
+ 
+                                                                                                                                                                                                                                                                                                      
+import random
+x=random.randint(1,10)
+y=input('1到10猜1個數字')
+y=int(y)
+if x == y:
+    print('猜對了!')
+elif x > y:
+    print('太小了!')
+elif x < y:
+    print('太大了!')
+    
+    
+    
+    
+ 
+    
+
+
+import random
+x=random.randint(1,10)
+y=input('1到10猜1個數字')
+y=int(y
+while y!x :)
+    z=input('在猜一次!')
+    z=int(z)
+else:
+    print('對了!')
+    
+    
+      
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
